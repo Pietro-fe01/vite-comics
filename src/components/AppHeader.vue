4b6b7e9dd49f1b77cf35f4787c0a2b72201d26f1
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-24 16:00:35 +0100

    Creato componente AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,83 @@
 <script>
     export default {
         name: 'AppHeader',
+        data(){
+            return{
+                menuLinks: [
+                    {
+                        linkName: 'CHARACTERS',
+                    },
+                    {
+                        linkName: 'COMICS',
+                    },
+                    {
+                        linkName: 'MOVIES',
+                    },
+                    {
+                        linkName: 'TV',
+                    },
+                    {
+                        linkName: 'GAMES',
+                    },
+                    {
+                        linkName: 'COLLECTIBLES',
+                    },
+                    {
+                        linkName: 'VIDEOS',
+                    },
+                    {
+                        linkName: 'FANS',
+                    },
+                    {
+                        linkName: 'NEWS',
+                    },
+                    {
+                        linkName: 'SHOP',
+                    }
+                ]
+            }
+        }
     }
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <div>ciao</div>
+    <header class="container">
+        <div class="header__logo">
+            <a href="#">
+                <img src="../assets/dc-logo.png" alt="logo-DC">
+            </a>
         </div>
+        <nav class="header__nav">
+            <ul>
+                <li v-for="link in menuLinks"><a href="#">{{link.linkName}}</a></li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+
+        & .header__logo img{
+            max-width: 4.375rem;
+            margin: .9375rem 0;
+        }
+        & .header__nav ul{
+            height: 100%;
+            display: flex;
+            align-items: center;
+            list-style: none;
+        }
+        & .header__nav ul li{
+            margin-left: 1.5625rem;
+        }
+        & .header__nav ul li a{
+            color: black;
+            text-decoration: none;
+            font-weight: 500;
+            font-size: .8125rem;
+        }
+    }
 </style>
