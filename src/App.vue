8b0eb6fc65ecdfb49390d41d28488800b95b1de5
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-24 15:38:53 +0100

    Creata tutta la struttura da zero: Cominciamo!

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+
+  export default{
+    components: {
+      AppHeader,
+    }
+  }
+</script>
+
+<template>
+  <AppHeader/>
+</template>
+
+<style lang="scss">
+  @import './style/global.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        name: 'AppHeader',
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div>ciao</div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
